629915de362a4b80e18253f83331ae64060f03fa
Павел Егоров <pavel_egoroff@mail.ru>
2023-06-16 18:39:49 +0300

    Created a file to store mock data
    Added mock data for loading section

---
+++ js/data/data.js
@@ -0,0 +1,29 @@
+export const data = {
+  en: {
+    download: {
+      name: 'download',
+      title: {
+        priority: 1,
+        content: 'Peaceful shopping mindful money',
+      },
+      texts: [
+        'See how we can help with making your shopping experience and money management more ZEN.',
+      ],
+      image: {
+        source: '/assets/images/1.jpg',
+        description: 'Some description for image 1',
+      },
+      links: [
+        {
+          type: 'apple',
+          url: 'https://apple.com',
+        },
+        {
+          type: 'google',
+          url: 'https://google.com',
+        },
+      ],
+    },
+  },
+  ru: {},
+}
