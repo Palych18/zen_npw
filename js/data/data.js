6271903c904db185573e451eb5d43ceefb9158b2
Павел Егоров <pavel_egoroff@mail.ru>
2023-06-19 17:03:13 +0300

    Added mock data for clients section

--- js/data/data.js
+++ js/data/data.js
@@ -24,6 +24,47 @@ export const data = {
         },
       ],
     },
+    clients: {
+      name: 'clients',
+      title: {
+        priority: 2,
+        content: 'Clients',
+      },
+      brands: [
+        {
+          source: 'assets/clients/1.svg',
+          description: 'Some description for client 1',
+        },
+        {
+          source: 'assets/clients/2.svg',
+          description: 'Some description for client 2',
+        },
+        {
+          source: 'assets/clients/3.svg',
+          description: 'Some description for client 3',
+        },
+        {
+          source: 'assets/clients/4.svg',
+          description: 'Some description for client 4',
+        },
+        {
+          source: 'assets/clients/5.svg',
+          description: 'Some description for client 5',
+        },
+        {
+          source: 'assets/clients/6.svg',
+          description: 'Some description for client 6',
+        },
+        {
+          source: 'assets/clients/7.svg',
+          description: 'Some description for client 7',
+        },
+        {
+          source: 'assets/clients/8.svg',
+          description: 'Some description for client 8',
+        },
+      ],
+    },
   },
   ru: {},
 }
