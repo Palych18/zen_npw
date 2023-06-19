44cb754cc003a7d93c74afd70d9b16320639410a
Павел Егоров <pavel_egoroff@mail.ru>
2023-06-19 16:57:01 +0300

    Added mock data for cashback section

--- js/data/data.js
+++ js/data/data.js
@@ -24,6 +24,19 @@ export const data = {
         },
       ],
     },
+    cashback: {
+      name: 'cashback',
+      title: {
+        priority: 2,
+        content: 'and cashback!',
+      },
+      texts: [
+        'It saves a little bit of money while you’re shopping, but in many cases it takes ages to get anything out of it.',
+        'We gave it a thought and made cashback way more friendly.',
+        'Up to 14% of what you spend using ZEN Mastercard ® will materialize on your account right after the purchase.',
+      ],
+      buttonText: 'Order a consultation',
+    },
   },
   ru: {},
 }
