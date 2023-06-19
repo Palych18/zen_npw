34b320c3033e66f63da866a758d91455a47476f9
Павел Егоров <pavel_egoroff@mail.ru>
2023-06-19 16:48:39 +0300

    Added mock data for care section

--- js/data/data.js
+++ js/data/data.js
@@ -24,6 +24,22 @@ export const data = {
         },
       ],
     },
+    care: {
+      name: 'care',
+      title: {
+        priority: 2,
+        content: 'ZEN Care',
+      },
+      texts: [        
+        'Shopping online is usually a delightful experience. Until it\'s not.',
+        'Until something you buy, proves to be faulty, missing or a totally wrong item, you’re in for long product return procedures.',
+        'But, here comes ZEN Care: a free, built-in shopping protection.',
+      ],
+      image: {
+        source: 'assets/images/3.jpg',
+        description: 'Some description for image 3',
+      },
+    },
   },
   ru: {},
 }
