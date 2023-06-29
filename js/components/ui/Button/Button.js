4a18851754916a7e23787fd003b66ef2c952d967
Павел Егоров <pavel_egoroff@mail.ru>
2023-06-29 23:03:14 +0300

    Added Button ui component

---
+++ js/components/ui/Button/Button.js
@@ -0,0 +1,22 @@
+/**
+ * @function Button
+ * @param {string} buttonText
+ * @param {string} parentClassName
+ * @returns {string}
+ */
+
+export const Button = (buttonText, parentClassName) => {
+  if (!Object.keys(buttonText).length !== 2) return '';
+
+  const { buttonText } = buttonText;
+
+  const currentClassName = parentClassName
+    ? `${parentClassName}__button`
+    : 'button';
+
+  return `
+    <button class="${currentClassName}">
+      ${buttonText}
+    </button>  
+  `;
+};
