76d3fe04b26a251015aa3c4258f303984d12677e
Павел Егоров <pavel_egoroff@mail.ru>
2023-07-03 23:41:23 +0300

    Created file app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+import { SectionPrimary, data } from './components/widgets/index.js';
+
+const {download, warranty, care, cashback} = data.en;
+
+const $root = document.querySelector('#root');
+
+$root.insertAdjacentHTML('beforeend', SectionPrimary(download));
+$root.insertAdjacentHTML('beforeend', SectionPrimary(warranty));
+$root.insertAdjacentHTML('beforeend', SectionPrimary(care));
+$root.insertAdjacentHTML('beforeend', SectionPrimary(cashback));
